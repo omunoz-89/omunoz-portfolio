615f9ca20ad715652163c63dedf70204586fbf13
omunoz <steven.120189@gmail.com>
2021-07-08 18:13:12 -0400

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer">
+            <div class="footer-links navbar-start">
+      <a className="navbar-item" exact href="/">Email</a>
+      <a className="navbar-item"  href="https://github.com/omunoz-89">Github</a>
+      <a className="navbar-item"  href="https://www.linkedin.com/in/omar-munoz-056402130/">LinkedIn</a>
+      <a className="navbar-item"  href="/">Resume</a>
+      </div>
+        </footer>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Home = () => {
     return (
         <div className='animate__animated animate__fadeIn'>
+                <img src="https://res.cloudinary.com/sei412-om/image/upload/c_crop,h_2550,w_4032,x_0,y_0/v1625716944/B25C4C88-4251-4317-9FD2-D65E5DE838B4_e5xrdz.jpg" alt="me" className='animate__animated animate__fadeIn'/>
         <div className='layer'>
             <div className='welcome-text content is-medium'>
                 <h3><strong>Hello! The name is Omar Muñoz</strong></h3> <p>I am a full stack web developer and veteran with a passion for creating innovative, efficient solutions. With 5 years of military experience, I am resourceful, attentive to detail and can adapt to ambiguous situations. I have an endless hunger for self-improvement and challenge myself to write clean, precise code.</p>
             </div>
         </div>
-        <img src="https://res.cloudinary.com/sei412-om/image/upload/v1625716944/B25C4C88-4251-4317-9FD2-D65E5DE838B4_e5xrdz.jpg" alt="me" className='animate__animated animate__fadeIn'/>
         </div>
     )
 }
