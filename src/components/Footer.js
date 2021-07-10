11769f674e96bae441fd727b76107b8d780c4a1b
omunoz <steven.120189@gmail.com>
2021-07-09 22:13:19 -0400

    fixed footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import React from 'react'
 const Footer = () => {
     return (
     <div className="footer-nav navbar is-transparent">
-<a className='footer-links navbar-item' href="/">Email</a>
-<a className='footer-links navbar-item' href="https://github.com/omunoz-89">Github</a>
-<a className='footer-links navbar-item' href="https://www.linkedin.com/in/omar-munoz-056402130/">LinkedIn</a>
+<a className='footer-links navbar-item' href = "mailto: steven.120189@gmail.com">Email</a>
+<a className='footer-links navbar-item' href="https://github.com/omunoz-89" target="_blank" rel="noreferrer">Github</a>
+<a className='footer-links navbar-item' href="https://www.linkedin.com/in/omar-munoz-056402130/" target="_blank" rel="noreferrer">LinkedIn</a>
 <a className='footer-links navbar-item' href="/">Resume</a>
 </div>
     )
