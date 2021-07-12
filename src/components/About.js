b5075773d6d8395ea992397b0bf9120a10bee5aa
omunoz <steven.120189@gmail.com>
2021-07-12 12:00:36 -0400

    Added projects

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="content">
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,79 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
+import liveSus from '../media/live-sustainably.png'
+import meme from '../media/meme.png'
+import ss from '../media/ss.png'
 
 const Projects = () => {
     return (
-        <div>
-            <h1>Projects</h1>
+        <div className="projects columns is-multiline">
+
+
+    <div className='column is-4'>
+    <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src={ss} alt="Placeholder image"/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">Sneaky Snake</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris.
+    </div>
+  </div>
+</div>
+    </div>
+    <div className='column is-4'>
+    <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src={meme} alt="Placeholder image"/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">Meme Bank</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris.
+    </div>
+  </div>
+</div>
+    </div>
+    <div className='column is-4'>
+    <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src={liveSus} alt="Placeholder image"/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">Live Sustainably</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris.
+    </div>
+  </div>
+</div>
+    </div>
+
         </div>
     )
 }
